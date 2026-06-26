0bc01100a34a9c989333fb690b58b06fbaef88d2
Zengxu Yang <15492931+zengxu-yang@users.noreply.github.com>
2026-06-26 13:01:46 -0400

    Convert comment style

--- src/ch04/rtl8139_driver.c
+++ src/ch04/rtl8139_driver.c
@@ -8,7 +8,7 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Zengxu Yang");
 MODULE_DESCRIPTION("A simple RTL8139 PCI Ethernet driver from scratch.");
 
-// Realtek Vendor ID (0x10ec) and RTL8139 Device ID (0x8139)
+/* Realtek Vendor ID (0x10ec) and RTL8139 Device ID (0x8139) */
 static struct pci_device_id rtl8139_pci_tbl[] = {
 	{ PCI_DEVICE(0x10ec, 0x8139) },
 	{
@@ -17,22 +17,22 @@ static struct pci_device_id rtl8139_pci_tbl[] = {
 };
 MODULE_DEVICE_TABLE(pci, rtl8139_pci_tbl);
 
-// Called when the kernel finds an unhandled RTL8139 matching your ID
+/* Called when the kernel finds an unhandled RTL8139 matching your ID */
 static int rtl8139_pci_probe(struct pci_dev *pdev,
 			     const struct pci_device_id *id)
 {
 	printk(KERN_INFO "rtl8139: Hardware matched! Initializing...\n");
-	// TODO: Enable device, request regions, allocate net_device
+	/* TODO: Enable device, request regions, allocate net_device */
 	return 0;
 }
 
-// Called during rmmod or if the device is hot-unplugged
+/* Called during rmmod or if the device is hot-unplugged */
 static void rtl8139_pci_remove(struct pci_dev *pdev)
 {
 	printk(KERN_INFO "rtl8139: Driver unloaded, releasing resources.\n");
 }
 
-// PCI subsystem structure
+/* PCI subsystem structure */
 static struct pci_driver rtl8139_driver = {
 	.name = "rtl8139_driver",
 	.id_table = rtl8139_pci_tbl,
