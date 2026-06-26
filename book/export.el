2e2e252cd78c7d27cebf61344e97920ca5613bb4
Zengxu Yang <15492931+zengxu-yang@users.noreply.github.com>
2026-06-26 10:47:54 -0400

    Make the book more modular

--- book/export.el
+++ book/export.el
@@ -1,6 +1,13 @@
 (require 'ox-latex)
 
-;; We use double backslashes here so Emacs safely reads them as literal single backslashes
+;; ---- Create a local symlink for layout dependencies natively ----
+(let ((symlink-path "kernel-source")
+      (target-path "/home/zengxu/Projects/kernel-source-2.6.8"))
+  (when (file-exists-p symlink-path)
+    (delete-file symlink-path))
+  (make-symbolic-link target-path symlink-path))
+
+;; Define custom latex class
 (add-to-list 'org-latex-classes
              '("report-no-parts"
                "\\documentclass[12pt]{report}"
@@ -8,3 +15,23 @@
                ("\\section{%s}" . "\\section*{%s}")
                ("\\subsection{%s}" . "\\subsection*{%s}")
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+
+;; ---- PDF Export Options ----
+(setq org-latex-remove-logfiles nil)
+(setq org-latex-listings 'minted)
+(setq org-latex-minted-options
+      '(("frame" "lines")
+        ("fontsize" "\\footnotesize")
+        ("linenos" "true")
+        ("breaklines" "true")))
+
+(setq org-latex-pdf-process
+      '("latexmk -xelatex -silent -shell-escape -interaction=nonstopmode -output-directory=%o %f"))
+
+;; ---- Hooks ----
+
+;; 1. Untabify buffer before parsing
+(add-hook 'org-export-before-parsing-hook
+          (lambda (backend)
+            (setq tab-width 8)
+            (untabify (point-min) (point-max))))
