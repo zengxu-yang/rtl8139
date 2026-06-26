137bffa4f88332152c7dcf610666d36a089b5105
Zengxu Yang <15492931+zengxu-yang@users.noreply.github.com>
2026-06-26 03:12:34 -0400

    Navigate the kernel source

--- src/scripts/lsp/paths.sh
+++ src/scripts/lsp/paths.sh
@@ -5,4 +5,4 @@ sed -i 's|/usr/src/kernel-source-2.6.8|/home/zengxu/Projects/kernel-source-2.6.8
 sed -i 's|/home/zengxu/rtl8139|/home/zengxu/Projects/rtl8139/src/ch03|g' compile_commands.json
 
 # 3. Force the relative kernel include flags to be absolute host paths
-sed -i 's|-Iinclude|-I/home/zengxu/Projects/kernel-source-2.6.8/include|g' compile_commands.json
+# sed -i 's|-Iinclude|-I/home/zengxu/Projects/kernel-source-2.6.8/include|g' compile_commands.json
